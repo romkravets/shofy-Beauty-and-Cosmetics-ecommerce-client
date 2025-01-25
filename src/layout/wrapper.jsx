b74d48df5aed37a1e8849b43df5563a8363c9bd9
Roman Kravets <romann.kravets@gmail.com>
2025-01-25 16:03:07 +0200

    remove code

--- src/layout/wrapper.jsx
+++ src/layout/wrapper.jsx
@@ -1,45 +1,28 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, {useState, useEffect} from "react";
 import { ToastContainer } from "react-toastify";
 // internal
 import BackToTopCom from "@/components/common/back-to-top";
-import ProductModal from "@/components/common/product-modal";
-import {
-  get_cart_products,
-  initialOrderQuantity,
-} from "@/redux/features/cartSlice";
-import { get_wishlist_products } from "@/redux/features/wishlist-slice";
-import { get_compare_products } from "@/redux/features/compareSlice";
-import useAuthCheck from "@/hooks/use-auth-check";
 import Loader from "@/components/loader/loader";
 
 const Wrapper = ({ children }) => {
-  const { productItem } = useSelector((state) => state.productModal);
-  const dispatch = useDispatch();
-  const authChecked = useAuthCheck();
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    dispatch(get_cart_products());
-    dispatch(get_wishlist_products());
-    dispatch(get_compare_products());
-    dispatch(initialOrderQuantity());
-  }, [dispatch]);
+    setIsMounted(true);
+  }, []);
 
-  return !authChecked ? (
+  return !isMounted ? (
     <div
       className="d-flex align-items-center justify-content-center"
       style={{ height: "100vh" }}
     >
-      <Loader spinner="fade" loading={!authChecked} />
+      <Loader spinner="fade" loading={!isMounted} />
     </div>
   ) : (
     <div id="wrapper">
       {children}
       <BackToTopCom />
-      <ToastContainer />
-      {/* product modal start */}
-      {productItem && <ProductModal />}
-      {/* product modal end */}
+      {isMounted && <ToastContainer />} {/* ToastContainer only renders client-side */}
     </div>
   );
 };
