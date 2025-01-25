67388a69f56cc375fb7f4aff40ca8652d93ca440
Roman Kravets <romann.kravets@gmail.com>
2025-01-25 17:08:13 +0200

    clean assets

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,3 @@
-import store from "@/redux/store";
-import { Provider } from "react-redux";
 import ReactModal from "react-modal";
 import '../styles/index.scss';
 if (typeof window !== "undefined") {
@@ -12,10 +10,8 @@ if (typeof window !== "undefined") {
 
 export default function App({ Component, pageProps }) {
   return (
-      <Provider store={store}>
-          <div id="root">
-            <Component {...pageProps} />
-          </div>
-      </Provider>
+      <div id="root">
+        <Component {...pageProps} />
+      </div>
   )
 }
